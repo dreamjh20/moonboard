9b5c6cf98bdce76716c089a4af1311ed17fdb9b0
dreamjh20 <dreamjh20@gmail.com>
2022-03-21 19:53:31 +0900

    CREATE: Moonboard

---
+++ src/main/java/com/moon/moonboard/MoonboardApplication.java
@@ -0,0 +1,13 @@
+package com.moon.moonboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoonboardApplication.class, args);
+    }
+
+}
