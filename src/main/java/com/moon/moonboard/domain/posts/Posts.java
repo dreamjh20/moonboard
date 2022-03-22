88a4183867427b9e47f9355128a8b494bcb2facf
dreamjh20 <dreamjh20@gmail.com>
2022-03-22 20:22:49 +0900

    CREATE: Posts.java entity class

---
+++ src/main/java/com/moon/moonboard/domain/posts/Posts.java
@@ -0,0 +1,24 @@
+package com.moon.moonboard.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Getter
+@NoArgsConstructor
+
+//will be linked with table
+@Entity
+//in Entity class never makes Setter method
+
+public class Posts {
+
+
+}
