9eb61b91f131d7dfbdee822ee02b70d428287b2a
dreamjh20 <dreamjh20@gmail.com>
2022-03-22 20:23:52 +0900

    ADD: Board Columns set title, content, author

--- src/main/java/com/moon/moonboard/domain/posts/Posts.java
+++ src/main/java/com/moon/moonboard/domain/posts/Posts.java
@@ -20,5 +20,25 @@ import javax.persistence.Id;
 
 public class Posts {
 
+    //primary key
+    @Id
+    //primary key rule
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    //make builder itself
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
 }
