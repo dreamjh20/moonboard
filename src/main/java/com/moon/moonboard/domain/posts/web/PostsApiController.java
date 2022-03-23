080d5e4ac949dea826c76667772aa0c20ca54c71
dreamjh20 <dreamjh20@gmail.com>
2022-03-23 23:04:11 +0900

    ADD: Post Api

--- src/main/java/com/moon/moonboard/domain/posts/web/PostsApiController.java
+++ src/main/java/com/moon/moonboard/domain/posts/web/PostsApiController.java
@@ -1,5 +1,21 @@
 package com.moon.moonboard.domain.posts.web;
 
+import com.moon.moonboard.domain.posts.web.dto.PostsSaveRequestDto;
+import com.moon.moonboard.service.posts.PostsService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RequiredArgsConstructor
+@RestController
 public class PostsApiController {
 
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
 }
