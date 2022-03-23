3532964fd167fb14f78e3c692f15ffb67c6b5aa2
dreamjh20 <dreamjh20@gmail.com>
2022-03-23 23:07:10 +0900

    ADD: Control PostsRepository with transactional

---
+++ src/main/java/com/moon/moonboard/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.moon.moonboard.service.posts;
+
+import com.moon.moonboard.domain.posts.PostsRepository;
+import com.moon.moonboard.domain.posts.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+//Constructor Injection
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
