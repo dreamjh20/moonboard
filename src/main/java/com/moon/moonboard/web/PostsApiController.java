b42957608fe2b6face1a2e7461eb5186e381c768
dreamjh20 <dreamjh20@gmail.com>
2022-03-28 23:35:36 +0900

    ADD: Posts update

--- src/main/java/com/moon/moonboard/web/PostsApiController.java
+++ src/main/java/com/moon/moonboard/web/PostsApiController.java
@@ -1,12 +1,13 @@
 package com.moon.moonboard.web;
 
+import com.moon.moonboard.domain.posts.PostsRepository;
+import com.moon.moonboard.web.dto.PostsResponseDto;
 import com.moon.moonboard.web.dto.PostsSaveRequestDto;
 import com.moon.moonboard.service.posts.PostsService;
+import com.moon.moonboard.web.dto.PostsUpdateRequestDto;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,4 +19,16 @@ public class PostsApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id)
+    {
+        return postsService.findById(id);
+    }
+
 }
