58f730514343989d097f5a9397faf997bd19838b
dreamjh20 <dreamjh20@gmail.com>
2022-03-28 23:38:23 +0900

    ADD: Update Method

--- src/main/java/com/moon/moonboard/service/posts/PostsService.java
+++ src/main/java/com/moon/moonboard/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.moon.moonboard.service.posts;
 
+import com.moon.moonboard.domain.posts.Posts;
 import com.moon.moonboard.domain.posts.PostsRepository;
+import com.moon.moonboard.web.dto.PostsResponseDto;
 import com.moon.moonboard.web.dto.PostsSaveRequestDto;
+import com.moon.moonboard.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,7 +17,21 @@ public class PostsService {
     private final PostsRepository postsRepository;
 
     @Transactional
-    public long save(PostsSaveRequestDto requestDto) {
+    public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("Post doesn't exist! id="+id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("Post doesn't exist! id="+id));
+
+        return new PostsResponseDto(entity);
     }
 }
