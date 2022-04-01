a1ff78842cbf8617e533d2e0894c362b35d6ae15
dreamjh20 <dreamjh20@gmail.com>
2022-04-01 09:25:18 +0900

    ADD: BaseTimeEntity Regist Test print created datetime, modified datetime

--- src/test/java/com/moon/moonboard/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/moon/moonboard/domain/posts/PostsRepositoryTest.java
@@ -2,6 +2,7 @@ package com.moon.moonboard.domain.posts;
 
 import com.moon.moonboard.MoonboardApplication;
 
+import org.assertj.core.util.Lists;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -45,6 +47,25 @@ public class PostsRepositoryTest {
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
 
+    }
+
+    @Test
+    public void regist_BaseTimeEntity() {
+        LocalDateTime now = LocalDateTime.of(2022,4, 1, 9, 15, 20);
+        postsRepository.save(Posts.builder()
+                .title("test_title")
+                .content("test_content")
+                .author("test_author")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>> createdDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 
 }
