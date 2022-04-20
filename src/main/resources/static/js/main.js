9736172fc18e2e807bf13d22fd2492b680a1b177
dreamjh20 <dreamjh20@gmail.com>
2022-04-20 11:26:14 +0900

    UPDATE: Null Check Algorithm

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,39 +1,37 @@
-let flag = 0
 const main = {
     init: function () {
         const _this = this;
         $('#btn-save').on('click', function () {
-            flag = 0;
+            let flag = 0;
 
-            //버튼 이벤트 발생 시 제목이 비었는지 확인
-            const title = $('#title').val();
-            if (title.replace(/\s|　/gi, "").length === 0) {
-                alert('OH Title Is Empty!')
-                console.log("Null Title")
-                $('#title').focus();
-                flag = 1
+            let arr = ['#title', '#author', '#content']
+            for (let i = 0; i < arr.length; i++) {
+                let input = arr[i]
+                let ipt = $(input).val()
+                if (ipt.replace(/\s|　/gi, "").length === 0)
+                    flag = i + 1
             }
 
-            //버튼 이벤트 발생 시 작성자가 비었는지 확인
-            const author = $('#author').val();
-            if (author.replace(/\s|　/gi, "").length === 0) {
-                alert('OH Author Is Empty!')
-                console.log("Null Author")
-                $('#author').focus();
-                flag = 1
-            }
-
-            //버튼 이벤트 발생 시 내용이 비었는지 확인
-            const content = $('#content').val();
-            if (content.replace(/\s|　/gi, "").length === 0) {
-                alert('OH Content Is Empty!')
-                console.log("Null Content")
-                $('#content').focus();
-                flag = 1
-            }
-            if(flag === 0) {
-                _this.save();
-                console.log("Clicked")
+            switch (flag) {
+                case 0:
+                    _this.save();
+                    console.log("Clicked")
+                    break
+                case 1:
+                    alert('OH Title Is Empty!')
+                    console.log("Null Title")
+                    $('#title').focus();
+                    break
+                case 2:
+                    alert('OH Author Is Empty!')
+                    console.log("Null Author")
+                    $('#author').focus();
+                    break
+                case 3:
+                    alert('OH Content Is Empty!')
+                    console.log("Null Content")
+                    $('#content').focus();
+                    break
             }
         });
     },
