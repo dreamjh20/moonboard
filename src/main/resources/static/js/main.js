557dc497134c73ac53ef2d2e4dfbab03ad85d5d8
dreamjh20 <dreamjh20@gmail.com>
2022-04-20 09:39:34 +0900

    FEAT: Null Author & Content Disable

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,10 +2,25 @@ const main = {
     init: function () {
         const _this = this;
         $('#btn-save').on('click', function () {
+
             //버튼 이벤트 발생 시 제목이 비었는지 확인
             const title = $("#title").val();
             if (title.replace(/\s|　/gi, "").length === 0) {
                 alert('OH Title Is Empty!')
+                $("$title").focus();
+            }
+
+            //버튼 이벤트 발생 시 작성자가 비었는지 확인
+            const author = $("#author").val();
+            if (author.replace(/\s|　/gi, "").length === 0) {
+                alert('OH Author Is Empty!')
+                $("$title").focus();
+            }
+
+            //버튼 이벤트 발생 시 내용이 비었는지 확인
+            const content = $("#content").val();
+            if (content.replace(/\s|　/gi, "").length === 0) {
+                alert('OH Content Is Empty!')
                 $("$title").focus();
             }
 
