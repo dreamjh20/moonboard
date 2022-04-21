7a64ac48ecb99d9506eea86e65be65fed854c631
dreamjh20 <dreamjh20@gmail.com>
2022-04-21 19:01:14 +0900

    ADD: Get Post Query

--- src/main/java/com/moon/moonboard/domain/posts/PostsRepository.java
+++ src/main/java/com/moon/moonboard/domain/posts/PostsRepository.java
@@ -1,7 +1,12 @@
 package com.moon.moonboard.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long>{
 
+    @Query("SELECT pst FROM Posts pst ORDER BY pst.id DESC")
+    List<Posts> findAllDesc();
 }
