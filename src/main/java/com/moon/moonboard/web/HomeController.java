c0f47f1246d500a50972cbc6f7c37ea9ad45217e
dreamjh20 <dreamjh20@gmail.com>
2022-04-21 19:18:28 +0900

    FEAT: Get Posts from Service send to view

--- src/main/java/com/moon/moonboard/web/HomeController.java
+++ src/main/java/com/moon/moonboard/web/HomeController.java
@@ -1,13 +1,20 @@
 package com.moon.moonboard.web;
 
+import com.moon.moonboard.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "home";
     }
 
