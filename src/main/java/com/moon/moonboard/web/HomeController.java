85c6ea127895265fb352a88aa3afef8042e6196b
dreamjh20 <dreamjh20@gmail.com>
2022-04-22 10:52:11 +0900

    FEAT: Map Update Method

--- src/main/java/com/moon/moonboard/web/HomeController.java
+++ src/main/java/com/moon/moonboard/web/HomeController.java
@@ -1,17 +1,18 @@
 package com.moon.moonboard.web;
 
 import com.moon.moonboard.service.posts.PostsService;
+import com.moon.moonboard.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
 public class HomeController {
 
     private final PostsService postsService;
-
 
     @GetMapping("/")
     public String home(Model model) {
@@ -22,5 +23,13 @@ public class HomeController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
