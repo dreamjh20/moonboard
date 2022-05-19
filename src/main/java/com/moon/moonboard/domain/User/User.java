8ad5f2c0df78d46c7d694efc69537c64e246562a
dreamjh20 <dreamjh20@gmail.com>
2022-05-19 18:56:14 +0900

    CREATE: User.java

---
+++ src/main/java/com/moon/moonboard/domain/User/User.java
@@ -0,0 +1,13 @@
+package com.moon.moonboard.domain.User;
+
+import com.moon.moonboard.domain.BaseTimeEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+}
