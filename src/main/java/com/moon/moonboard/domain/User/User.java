cef6941019134785f37f2dc3c65d21c61f5a8663
dreamjh20 <dreamjh20@gmail.com>
2022-05-19 18:58:53 +0900

    ADD: Name Column

--- src/main/java/com/moon/moonboard/domain/User/User.java
+++ src/main/java/com/moon/moonboard/domain/User/User.java
@@ -4,10 +4,7 @@ import com.moon.moonboard.domain.BaseTimeEntity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
@@ -17,4 +14,7 @@ public class User extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false)
+    private String name;
 }
