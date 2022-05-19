6d4a80e336fe1d17ee7f48ff67fd545422a5496e
dreamjh20 <dreamjh20@gmail.com>
2022-05-19 18:58:21 +0900

    ADD: Id AS PK

--- src/main/java/com/moon/moonboard/domain/User/User.java
+++ src/main/java/com/moon/moonboard/domain/User/User.java
@@ -5,9 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @NoArgsConstructor
 @Entity
 public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 }
