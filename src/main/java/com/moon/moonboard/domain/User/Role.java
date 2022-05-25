90b774cf223623e0d41836b775b99dcc28bd1189
dreamjh20 <dreamjh20@gmail.com>
2022-05-25 15:50:00 +0900

    ADD: GUEST & USER Role guest and user

--- src/main/java/com/moon/moonboard/domain/User/Role.java
+++ src/main/java/com/moon/moonboard/domain/User/Role.java
@@ -6,5 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Role {
+    GUEST("ROLE_GUEST", "Guest"),
+    USER("ROLE_USER", "User");
 
+    private final String key;
+    private final String title;
 }
