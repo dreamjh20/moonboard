ff137fbf35473765805816022efdafadd96636e1
dreamjh20 <dreamjh20@gmail.com>
2022-05-31 11:39:58 +0900

    CREATE: SecurityConfig.java

---
+++ src/main/java/com/moon/moonboard/config/oauth/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.moon.moonboard.config.oauth;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
