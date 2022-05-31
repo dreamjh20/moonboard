2562484d062bfba51a1f1d5b33c181f5692f48d3
dreamjh20 <dreamjh20@gmail.com>
2022-05-31 16:09:51 +0900

    ADD: Configure Method

--- src/main/java/com/moon/moonboard/config/oauth/SecurityConfig.java
+++ src/main/java/com/moon/moonboard/config/oauth/SecurityConfig.java
@@ -1,7 +1,34 @@
 package com.moon.moonboard.config.oauth;
 
+import com.moon.moonboard.domain.User.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.userinfo.CustomUserTypesOAuth2UserService;
 
+@RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final CustomUserTypesOAuth2UserService customUserTypesOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeHttpRequests()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name()).anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                            .userService(customUserTypesOAuth2UserService);
+
+    }
 }
