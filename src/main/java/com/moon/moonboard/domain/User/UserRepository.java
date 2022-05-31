aae5b83d84f97501d4f50b9e946165ee7482300e
dreamjh20 <dreamjh20@gmail.com>
2022-05-31 10:14:32 +0900

    UPDATE: Interface

--- src/main/java/com/moon/moonboard/domain/User/UserRepository.java
+++ src/main/java/com/moon/moonboard/domain/User/UserRepository.java
@@ -2,6 +2,7 @@ package com.moon.moonboard.domain.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
