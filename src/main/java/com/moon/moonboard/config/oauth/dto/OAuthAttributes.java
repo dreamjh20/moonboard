0bd93b6edcec41366da5e5c25528de0baa2c3dd8
dreamjh20 <dreamjh20@gmail.com>
2022-05-31 19:10:40 +0900

    ADD: of OAuthAttributes Method

--- src/main/java/com/moon/moonboard/config/oauth/dto/OAuthAttributes.java
+++ src/main/java/com/moon/moonboard/config/oauth/dto/OAuthAttributes.java
@@ -1,4 +1,10 @@
 package com.moon.moonboard.config.oauth.dto;
 
+import java.util.Map;
+
 public class OAuthAttributes {
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
 }
