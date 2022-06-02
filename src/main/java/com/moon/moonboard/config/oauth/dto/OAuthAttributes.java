1b9d12bc78f6c061b082d51ed3a0b7ccdf88e02e
dreamjh20 <dreamjh20@gmail.com>
2022-06-02 19:08:33 +0900

    UPDATE: Access Modifier

--- src/main/java/com/moon/moonboard/config/oauth/dto/OAuthAttributes.java
+++ src/main/java/com/moon/moonboard/config/oauth/dto/OAuthAttributes.java
@@ -29,7 +29,7 @@ public class OAuthAttributes {
         return ofGoogle(userNameAttributeName, attributes);
     }
 
-    public static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
