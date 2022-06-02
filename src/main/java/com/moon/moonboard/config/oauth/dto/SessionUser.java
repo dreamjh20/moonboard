76d1960a73469a385af914473c0b30b474356154
dreamjh20 <dreamjh20@gmail.com>
2022-06-02 19:07:00 +0900

    ADD: Serializable

--- src/main/java/com/moon/moonboard/config/oauth/dto/SessionUser.java
+++ src/main/java/com/moon/moonboard/config/oauth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.moon.moonboard.config.oauth.dto;
 import com.moon.moonboard.domain.User.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
